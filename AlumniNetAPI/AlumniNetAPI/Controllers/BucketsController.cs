d255846061a0015873eae34a96ff62a87e12aef7
Mihai Oprea <moprea@talentingsoftware.com>
2023-04-30 11:12:41 +0300

    Deleted file enpoints from bucket controller

--- AlumniNetAPI/AlumniNetAPI/Controllers/BucketsController.cs
+++ AlumniNetAPI/AlumniNetAPI/Controllers/BucketsController.cs
@@ -36,68 +36,6 @@ namespace AlumniNetAPI.Controllers
         {
             await _s3Client.DeleteBucketAsync(bucketName);
             return NoContent();
-        }
-
-        [HttpPost("UploadFile")]
-        public async Task<IActionResult> UploadFileAsync(IFormFile file, string bucketName, string? prefix)
-        {
-            var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
-            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
-            var request = new PutObjectRequest()
-            {
-                BucketName = bucketName,
-                Key = string.IsNullOrEmpty(prefix) ? file.FileName : $"{prefix?.TrimEnd('/')}/{file.FileName}",
-                InputStream = file.OpenReadStream()
-            };
-            request.Metadata.Add("Content-Type", file.ContentType);
-            await _s3Client.PutObjectAsync(request);
-            return Ok($"File {prefix}/{file.FileName} uploaded to S3 successfully!");
-        }
-
-        [HttpGet("GetAllFiles")]
-        public async Task<IActionResult> GetAllFilesAsync(string bucketName, string? prefix)
-        {
-            var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
-            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
-            var request = new ListObjectsV2Request()
-            {
-                BucketName = bucketName,
-                Prefix = prefix
-            };
-            var result = await _s3Client.ListObjectsV2Async(request);
-            var s3Objects = result.S3Objects.Select(s =>
-            {
-                var urlRequest = new GetPreSignedUrlRequest()
-                {
-                    BucketName = bucketName,
-                    Key = s.Key,
-                    Expires = DateTime.UtcNow.AddMinutes(1)
-                };
-                return new S3ObjectDto()
-                {
-                    Name = s.Key.ToString(),
-                    PresignedUrl = _s3Client.GetPreSignedURL(urlRequest),
-                };
-            });
-            return Ok(s3Objects);
-        }
-
-        [HttpGet("GetFileByKey")]
-        public async Task<IActionResult> GetFileByKeyAsync(string bucketName, string key)
-        {
-            var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
-            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
-            var s3Object = await _s3Client.GetObjectAsync(bucketName, key);
-            return File(s3Object.ResponseStream, s3Object.Headers.ContentType);
-        }
-
-        [HttpDelete("DeleteFileByKey")]
-        public async Task<IActionResult> DeleteFileByKeyAsync(string bucketName, string key)
-        {
-            var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
-            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist");
-            await _s3Client.DeleteObjectAsync(bucketName, key);
-            return NoContent();
-        }
+        }        
     }
 }
