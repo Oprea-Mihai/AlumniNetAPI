9b6bb9763c8690fac579e837b9aff65923ff71be
Mihai Oprea <moprea@talentingsoftware.com>
2023-05-19 21:57:50 +0300

    Notification controller

---
+++ AlumniNetAPI/AlumniNetAPI/Controllers/NotificationController.cs
@@ -0,0 +1,21 @@
+﻿using AlumniNetAPI.Repository.Interfaces;
+using AlumniNetAPI.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlumniNetAPI.Controllers
+{
+    public class NotificationController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public NotificationController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+          
+                _unitOfWork = unitOfWork;
+                _mapper = mapper;
+             
+        }
+    }
+}
